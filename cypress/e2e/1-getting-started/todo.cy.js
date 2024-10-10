023204d8929b554604529faef8a1b3dd301fc46b
muzamilmemon1 <muhammadmuzamilhussain5@gmail.com>
2024-10-10 22:21:29 +0500

    edit: update url

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -1,41 +1,41 @@
-describe('Todo App', () => {
+describe("Todo App", () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000');
+    cy.visit("http://localhost:5173");
   });
 
-  it('should add a new task', () => {
-    cy.get('input[placeholder="Add a task here..."]').type('Test Task{enter}');
-    cy.get('.bg-sky-100').should('contain', 'Test Task');
+  it("should add a new task", () => {
+    cy.get('input[placeholder="Add a task here..."]').type("Test Task{enter}");
+    cy.get(".bg-sky-100").should("contain", "Test Task");
   });
 
-  it('should complete a task', () => {
-    cy.get('input[placeholder="Add a task here..."]').type('Test Task{enter}');
+  it("should complete a task", () => {
+    cy.get('input[placeholder="Add a task here..."]').type("Test Task{enter}");
     cy.get('.material-symbols-outlined:contains("done")').click();
-    cy.get('.bg-blue-500').should('contain', 'Test Task');
+    cy.get(".bg-blue-500").should("contain", "Test Task");
   });
 
-  it('should mark a completed task as incomplete', () => {
-    cy.get('input[placeholder="Add a task here..."]').type('Test Task{enter}');
+  it("should mark a completed task as incomplete", () => {
+    cy.get('input[placeholder="Add a task here..."]').type("Test Task{enter}");
     cy.get('.material-symbols-outlined:contains("done")').click(); // Mark as done
     cy.get('.material-symbols-outlined:contains("close")').click(); // Mark as incomplete
-    cy.get('.bg-sky-100').should('contain', 'Test Task');
+    cy.get(".bg-sky-100").should("contain", "Test Task");
   });
 
-  it('should edit a task', () => {
-    cy.get('input[placeholder="Add a task here..."]').type('Test Task{enter}');
+  it("should edit a task", () => {
+    cy.get('input[placeholder="Add a task here..."]').type("Test Task{enter}");
     cy.get('.material-symbols-outlined:contains("edit")').click(); // Edit task
-    cy.get('input').clear().type('Updated Task{enter}');
-    cy.get('.bg-sky-100').should('contain', 'Updated Task');
+    cy.get("input").clear().type("Updated Task{enter}");
+    cy.get(".bg-sky-100").should("contain", "Updated Task");
   });
 
-  it('should remove a task', () => {
-    cy.get('input[placeholder="Add a task here..."]').type('Test Task{enter}');
+  it("should remove a task", () => {
+    cy.get('input[placeholder="Add a task here..."]').type("Test Task{enter}");
     cy.get('.material-symbols-outlined:contains("close")').click();
-    cy.get('.bg-sky-100').should('not.exist');
+    cy.get(".bg-sky-100").should("not.exist");
   });
 
-  it('should not add an empty task', () => {
-    cy.get('input[placeholder="Add a task here..."]').type('{enter}');
-    cy.get('.bg-sky-100').should('not.exist');
+  it("should not add an empty task", () => {
+    cy.get('input[placeholder="Add a task here..."]').type("{enter}");
+    cy.get(".bg-sky-100").should("not.exist");
   });
 });
